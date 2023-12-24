f199ce06522b27cfd403bb7d816e103e60ca0a31
quochuy232005 <144077996+TranHuuQuocHuy@users.noreply.github.com>
2023-12-24 15:44:58 +0700

    Add files via upload

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,18 @@
+package basic.dev;
+
+/**
+ * Syntax
+ * 
+ * @author Trần Hữu Quốc Huy
+ *
+ */
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		int number;
+		String nameSchool;
+	}
+}
